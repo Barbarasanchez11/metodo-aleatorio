9ec44261d9316c0a232c7e256ab46ed85112d0e3
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-07-31 00:36:37 +0200

    metodo aleatorio

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let metodos = [
+    "filter", "map", "forEach", "flat",
+    "find", "indexOf", "includes", "split",
+    "join", "reduce", "reverse", "slice"
+];
+
+document.getElementById("generarEjercicio").addEventListener("click", () => {
+    
+    let longitud = metodos.length;
+    
+     
+        let metodoAleatorio = metodos[Math.floor(Math.random() * longitud)];
+
+
+    document.getElementById("result").innerHTML = `<p>Ejercicio generado: ${metodoAleatorio}</p>`;
+});
